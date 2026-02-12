738cfd452c423baf0a12d2be9227071f818db699
adithya <adithyasivaraman5@gmail.com>
2026-02-12 15:28:08 +0530

    [Adithya] Added : UC1 : Display Puzzle Title

---
+++ src/LexicalTwistPuzzle.java
@@ -0,0 +1,16 @@
+/**
+ * Lexical Twist Puzzle Application
+ *
+ * UC1: Display Puzzle Title
+ *
+ * Prints application title to console.
+ *
+ * @author Adithya
+ * @version 1.0
+ */
+public class LexicalTwistPuzzle {
+
+    public static void main(String[] args) {
+        System.out.println("Lexical Twist Puzzle");
+    }
+}
